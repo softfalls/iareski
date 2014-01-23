dbc0269712c9128edb6ab96500553d1efbfb7126
Jgasparoto <gaspa_splinter@hotmail.com>
2014-01-23 12:33:43 -0800

    modificacao cabeçalho

--- index.php
+++ index.php
@@ -114,7 +114,7 @@
 </div>
 <!-- End of Splash Screen -->
 
-<div id="cab" style="height: 10%; background: #535959; margin-left: 0px; margin-right: -20px;">
+<div id="cab" style="height: 10%; background: #7F8289; margin-left: 0px; margin-right: -20px;">
     <img title="iareski" src="imagens/logooficialempresa.png" alt="iareski_logo" style="width: 300px; margin-left:100px">
     <div style="margin-top: -65px; color: #FFFFFF; font: 900 20px 'Roboto',sans-serif; text-shadow: 0 2px 0 #000000; width: 250px; margin-left:800px"> 
             <p> EDMILSON IARESKI (45) 3574-7043 </p>
